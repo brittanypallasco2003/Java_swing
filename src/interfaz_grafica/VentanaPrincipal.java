06c39501fc767019b81ab21de4aeb6ea24656411
brittanypallasco2003 <nohemiespinel0@gmail.com>
2023-07-08 12:41:49 -0500

    se añade funcionalidad al checkbox

--- src/interfaz_grafica/VentanaPrincipal.java
+++ src/interfaz_grafica/VentanaPrincipal.java
@@ -30,6 +30,7 @@ public class VentanaPrincipal {
     GENERA un constructor en la clase que hace referencia el form
      */
 
+    //BOTON VENTANA EMERGENTE----
     public VentanaPrincipal() {
         botonVentanaEmergente.addActionListener(new ActionListener() {
             @Override
@@ -39,20 +40,45 @@ public class VentanaPrincipal {
                 JOptionPane.showMessageDialog(null, "Hola Mundo");
             }
         });
+
+        //BOTON BIENVENIDA---
         bienvenidaUsuarioboton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //CAPTURAR LO QUE EL USUARIO INGRESE
                 String nombreUsuario = inputNombreUsuario.getText();//llamamos al elemento text field de la interfaz grafica seguido del getText-->se OBTIENE LO QUE INGRESA el usuario, y se guarda en una variable
-                String mensajeBienvenida="Bienvenido"+nombreUsuario+" ! :)";
-                mensajeLabelUsuario.setText(mensajeBienvenida);//seteamos el texto que se muestra como label para que se muestre en la ventana
+                String mensajeBienvenida="Bienvenido "+nombreUsuario+" ! :)";
+                inputNombreUsuario.setText(mensajeBienvenida);//seteamos el texto que se muestra como label para que se muestre en la ventana
             }
         });
+
+
+        //BOTON AUTENTICACION USUARIO---
         ingresarboton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String usuario=textField1.getText();
                 String password=String.valueOf(passwordField1.getPassword());
+                if(usuario.equals("bri")&& password.equals("123")){
+                    JOptionPane.showMessageDialog(null,"Usuario Identificado");
+                }else {
+                    JOptionPane.showMessageDialog(null,"Error en usuario y clave");
+                }
+            }
+        });
+
+        //CHECKBOX-----
+        seePassowrdCheckBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            //seePassowrdCheckBox.isSelected();//retorna true si esta seleccionado, false si no lo esta
+                if(seePassowrdCheckBox.isSelected()){
+                    passwordField1.setEchoChar((char)0);//para el caso de mostrar. Como setEchoChar espera un char, se le manda un 0
+                }else {
+                    passwordField1.setEchoChar('*');//si el check esta desabilitado se llenara el texfield de *
+                                                    //setea caul caracter va a utilizar para ocultar la contraseña
+                }
+
             }
         });
     }
